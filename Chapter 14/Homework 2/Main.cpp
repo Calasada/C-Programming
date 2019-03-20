e83a17db34e80b5d5798a0784abe104c40b59b30
Calasada <carlyodes1@gmail.com>
2019-03-20 17:55:58 -0400

    Chapter 14 Homework 2 [FINISHED]

--- Chapter 14/Homework 2/Main.cpp
+++ Chapter 14/Homework 2/Main.cpp
@@ -5,5 +5,11 @@ using namespace std;
 
 int main() {
 
+   complexType c1(6, 3), c2(7, -5);
+
+   complexType c3 = c1/c2;
+   c3.print();
+   cout << endl;
+
    return 0;
 }
--- Chapter 14/Homework 2/complexType.h
+++ Chapter 14/Homework 2/complexType.h
@@ -1,6 +1,7 @@
 #ifndef H_complexNumber
 #define H_complexNumber
 #include <iostream>
+#include <cmath>
 using namespace std;
 class complexType {
   // Overload the stream insertion and extraction operators
@@ -20,14 +21,23 @@ class complexType {
   // Initializes the complex number according to
   // the parameters.
   // Postcondition: realPart = real; imaginaryPart = imag;
-  complexType operator+(const complexType& otherComplex) const;
-  // Overload the operator +
-  complexType operator*(const complexType& otherComplex) const;
-  // Overload the operator *
-  bool operator==(const complexType& otherComplex) const;
-  // Overload the operator ==
-  complexType operator-(const complexType&) const;
-  complexType operator/(const complexType&) const;
+  void print();
+  // complexType operator+(const complexType& otherComplex) const;
+  // // Overload the operator +
+  // complexType operator*(const complexType& otherComplex) const;
+  // // Overload the operator *
+  // bool operator==(const complexType& otherComplex) const;
+  // // Overload the operator ==
+  // complexType operator-(const complexType&) const;
+  // complexType operator/(const complexType&) const;
+
+  friend complexType operator+(complexType&, complexType&);
+  friend complexType operator-(complexType&, complexType&);
+  friend complexType operator*(complexType&, complexType&);
+  friend complexType operator/(complexType&, complexType&);
+  friend bool operator==(complexType&, complexType&);
+  friend bool operator<(complexType&, complexType&);
+  friend bool operator>(complexType&, complexType&);
  private:
   double realPart;       // variable to store the real part
   double imaginaryPart;  // variable to store the
@@ -43,9 +53,7 @@ istream& operator>>(istream& isObject, complexType& complex) {
   isObject >> ch;                     // Step e
   return isObject;                    // return the istream object
 }
-bool complexType::operator==(const complexType& otherComplex) const {
-  return (realPart == otherComplex.realPart && imaginaryPart == otherComplex.imaginaryPart);
-}
+
 // Constructor
 complexType::complexType(double real, double imag) {
   realPart = real;
@@ -61,29 +69,79 @@ void complexType::getComplex(double& real, double& imag) const {
   real = realPart;
   imag = imaginaryPart;
 }
-// overload the operator +
-complexType complexType::operator+(const complexType& otherComplex) const {
+// bool complexType::operator==(const complexType& otherComplex) const {
+//   return (realPart == otherComplex.realPart && imaginaryPart == otherComplex.imaginaryPart);
+// }
+// // overload the operator +
+// complexType complexType::operator+(const complexType& otherComplex) const {
+//   complexType temp;
+//   temp.realPart = realPart + otherComplex.realPart;
+//   temp.imaginaryPart = imaginaryPart + otherComplex.imaginaryPart;
+//   return temp;
+// }
+// complexType complexType::operator*(const complexType& otherComplex) const {
+//   complexType temp;
+//   temp.realPart = (realPart * otherComplex.realPart) - (imaginaryPart * otherComplex.imaginaryPart);
+//   temp.imaginaryPart =
+//       (realPart * otherComplex.imaginaryPart) + (imaginaryPart * otherComplex.realPart);
+//   return temp;
+// }
+
+// complexType complexType::operator-(const complexType& c) const {
+//     double r1, i1, r2, i2;
+//     this->getComplex(r1, i1);
+//     c.getComplex(r2, i2);
+//     complexType nc(r1-r2, i1-i2);
+//     return nc;
+// }
+// complexType complexType::operator/(const complexType& c) const {
+//   double denom = pow(c.realPart, 2) + pow(c.imaginaryPart, 2);
+//   complexType temp1 = c;
+//   temp1.imaginaryPart *= -1;
+//   complexType temp2 = *this*temp1;
+//   temp2.realPart /= denom;
+//   temp2.imaginaryPart /= denom; 
+//   return temp2;
+// }
+
+complexType operator+(complexType& c1, complexType& c2) {
   complexType temp;
-  temp.realPart = realPart + otherComplex.realPart;
-  temp.imaginaryPart = imaginaryPart + otherComplex.imaginaryPart;
+  temp.realPart = c1.realPart + c2.realPart;
+  temp.imaginaryPart = c1.imaginaryPart + c2.imaginaryPart;
   return temp;
 }
-complexType complexType::operator*(const complexType& otherComplex) const {
+complexType operator-(complexType& c1, complexType& c2) {
+  double r1, i1, r2, i2;
+  c1.getComplex(r1, i1);
+  c2.getComplex(r2, i2);
+  complexType nc(r1-r2, i1-i2);
+  return nc;
+}
+complexType operator*(complexType& c1, complexType& c2) {
   complexType temp;
-  temp.realPart = (realPart * otherComplex.realPart) - (imaginaryPart * otherComplex.imaginaryPart);
-  temp.imaginaryPart =
-      (realPart * otherComplex.imaginaryPart) + (imaginaryPart * otherComplex.realPart);
+  temp.realPart = (c1.realPart * c2.realPart) - (c1.imaginaryPart * c2.imaginaryPart);
+  temp.imaginaryPart = (c1.realPart * c2.imaginaryPart) + (c1.imaginaryPart * c2.realPart);
   return temp;
 }
-
-complexType complexType::operator-(const complexType& c) const {
-    double r1, i1, r2, i2;
-    this->getComplex(r1, i1);
-    c.getComplex(r2, i2);
-    complexType nc(r1-r2, i1-i2);
-    return nc;
+complexType operator/(complexType& c1, complexType& c2) {
+  double denom = pow(c2.realPart, 2) + pow(c2.imaginaryPart, 2);
+  complexType temp1 = c2;
+  temp1.imaginaryPart *= -1;
+  complexType temp2 = c1*temp1;
+  temp2.realPart /= denom;
+  temp2.imaginaryPart /= denom; 
+  return temp2;
 }
-complexType complexType::operator/(const complexType& c) const {
-
+bool operator==(complexType& c1, complexType& c2) {
+  return (c1.realPart == c2.realPart && c1.imaginaryPart == c2.imaginaryPart);
+}
+bool operator<(complexType& c1, complexType& c2) {
+  return (c1.realPart < c2.realPart && c1.imaginaryPart < c2.imaginaryPart);
+}
+bool operator>(complexType& c1, complexType& c2) {
+ return (c1.realPart > c2.realPart && c1.imaginaryPart > c2.imaginaryPart);
+}
+void complexType::print() {
+  cout << realPart << " + " << imaginaryPart << "i";
 }
 #endif
