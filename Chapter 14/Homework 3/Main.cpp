de76444eed8a48ed60ffd2fbdb41d764a8e121d2
Carl Yoder <carlyodes1@gmail.com>
2019-04-03 08:00:12 -0400

    Chapter 14 Homework 3 [EXTRA]

--- Chapter 14/Homework 3/Main.cpp
+++ Chapter 14/Homework 3/Main.cpp
@@ -4,8 +4,19 @@
 using namespace std;
 
 int main() {
-    fractionType f;
-    cin >> f;
-    cout << f;
+
+    //Uses cin operator
+    fractionType f1, f2;
+    cout << "f1: " << endl;
+    cin >> f1;
+
+    cout << "f2: " << endl;
+    cin >> f2;
+
+    cout << "f1 + f2: " << (f1 + f2) << endl;
+    cout << "f1 - f2: " << (f1 - f2) << endl;
+    cout << "f1 * f2: " << (f1 * f2) << endl;
+    cout << "f1 / f2: " << (f1 / f2) << endl;
+
     return 0;
 }
