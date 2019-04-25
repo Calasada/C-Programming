40dd98c4d10c43c5ae7391aa332874566e4e7878
Carl Yoder <carlyodes1@gmail.com>
2019-04-25 11:25:49 -0400

    Chapter 17 Linked List

--- Chapter 17/Linked List/Main.cpp
+++ Chapter 17/Linked List/Main.cpp
@@ -7,11 +7,17 @@ int main() {
   unorderedLinkedList<int>* l = new unorderedLinkedList<int>;
 
   l->build(5);
+  cout << *l << endl;
   l->insertFirst(3);
+  cout << *l << endl;
   l->insertFirst(2);
+  cout << *l << endl;
   l->insertLast(5);
+  cout << *l << endl;
   l->deleteNode(3);
+  cout << *l << endl;
   l->insertItem(1, 5);
+  cout << *l << endl;
   l->deleteItem(0);
   cout << *l;
 
