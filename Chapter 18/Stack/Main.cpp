22c9b4b0ba4d6db86fa746999df416246ed7740a
Carl Yoder <carlyodes1@gmail.com>
2019-04-25 11:24:46 -0400

    Chapter 18 Stack

--- Chapter 18/Stack/Main.cpp
+++ Chapter 18/Stack/Main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
-#include <string>
 #include "linkedList.h"
+#include "stack.h"
 using namespace std;
 
 int main() {
-  unorderedLinkedList<int>* l = new unorderedLinkedList<int>;
 
-  l->build(5);
-  l->insertFirst(3);
-  l->insertFirst(2);
-  l->insertLast(5);
-  l->deleteNode(3);
-  l->insertItem(1, 5);
-  l->deleteItem(0);
-  cout << *l;
+  stack<int> *s = new stack<int>;
+
+  s->push(5);
+  s->pop();
+
+  cout << *s;
 
   return 0;
 }
--- Chapter 18/Stack/stack.h
+++ Chapter 18/Stack/stack.h
@@ -10,13 +10,17 @@ public:
     ~stack();
     void push(const type&);
     void pop();
+
+    template<class type2>
+    friend ostream& operator<<(ostream&, const stack<type2>&);
+
 private:
-    unorderedLinkedList<type>* list = new unorderedLinkedList<type>;
+    unorderedLinkedList<type>* list;
 };
 
 template<class type>
 stack<type>::stack() {
-    list->list();
+    list = new unorderedLinkedList<type>;
 }
 
 template<class type>
@@ -32,6 +36,12 @@ void stack<type>::push(const type& newInfo) {
 template<class type>
 void stack<type>::pop() {
     list->deleteItem(0);
+}
+
+template<class type>
+ostream& operator<<(ostream& out, const stack<type>& s) {
+    out << *s.list;
+    return out;
 }
 
 #endif
